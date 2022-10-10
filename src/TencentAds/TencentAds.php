b5adf7b83f6317a35412b3e69abafad11550e276
tencent_ad_developer <tencent_ad_developer@tencent.com>
2022-10-10 11:55:13 +0800

    v1.1.43

--- src/TencentAds/TencentAds.php
+++ src/TencentAds/TencentAds.php
@@ -33,7 +33,7 @@ class TencentAds extends Tools
      * SDK version
      * @var string
      */
-    protected $version = '1.1.42';
+    protected $version = '1.1.43';
 
     /**
      * SDK version
